7dd0641a5ee9d7ad9bf4f7cb8382202fa2f9863d
LungeloTZungu <lzungu56@gmail.com>
2023-08-10 20:08:41 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace InsertingAnywhereInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArray = new int[6];
+            int length = 0;
+             // populating array
+            for (int i = 0; i<6; i++)
+            {
+                intArray[length] = i;
+                length++;
+            }
+            Console.WriteLine("Original Array:");
+            foreach(var i in intArray)
+            {
+                Console.WriteLine(i);
+            }
+
+            // Inserting at any point inan arrauy
+            for (int i = 4; i >= 2; i--)
+            {
+                // shifting each element one position to the right
+
+                intArray[i + 1] = intArray[i];
+
+            }
+            intArray[2] = 8;
+            Console.WriteLine("New Array:");
+            foreach(var i in intArray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
